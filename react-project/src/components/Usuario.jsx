bd4c3e41e3d8b513c39cc17addb98d489d780659
Victor Juan González <g.victor.juan@gmail.com>
2025-09-09 22:05:01 -0300

    dado en clase - React

---
+++ react-project/src/components/Usuario.jsx
@@ -0,0 +1,21 @@
+// function Usuario(props) {
+//   return (
+//     <div>
+//       <h2>{props.nombre}</h2>
+//       <p>Edad: {props.edad}</p>
+//       <p>Apellido: {props.apellido}</p>
+//     </div>
+//   );
+// }
+
+function Usuario({ nombre = 'ana', edad, apellido }) {
+  return (
+    <div>
+      <h2>{nombre}</h2>
+      <p>Edad: {edad}</p>
+      <p>Apellido: {apellido}</p>
+    </div>
+  );
+}
+
+export default Usuario;
